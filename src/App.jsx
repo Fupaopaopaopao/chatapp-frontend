f5f8ce99e3d9010eabf8eea65819a11917cd2695
Mac <fupaopao8483@gmail.com>
2026-02-03 21:36:24 +1100

    setting page

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,11 @@ import { useEffect } from 'react'
 import { Loader } from 'lucide-react'
 import Navbar from './components/Navbar'
 import { Toaster } from 'react-hot-toast'
+import { useThemeStore } from './store/useThemeStore'
 
 function App() {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore()
+  const { theme } = useThemeStore()
 
   useEffect(() => {
     checkAuth()
@@ -27,7 +29,7 @@ function App() {
   }
 
   return (
-    <>
+    <div className="h-screen" data-theme={theme}>
       <Navbar />
       <Routes>
         <Route
@@ -51,9 +53,9 @@ function App() {
           element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
         />
       </Routes>
-      
-        <Toaster />
-    </>
+
+      <Toaster />
+    </div>
   )
 }
 
